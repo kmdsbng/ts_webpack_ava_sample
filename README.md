1764be3e02a9372f4156b1c9ee90a212f73023b6
Yoshihiro Kameda <kameda.sbng@gmail.com>
2017-08-12 22:17:00 +0900

    able to build.

---
+++ README.md
@@ -0,0 +1,2 @@
+practice of http://qiita.com/noonworks/items/8bd35bc8bbd047abe206
+
---
+++ config/webpack.config.prod.js
@@ -0,0 +1,29 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  context: path.join(__dirname, '..'),
+  entry: './ts/index.ts',
+  output: {
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
+  ]
+}
+
